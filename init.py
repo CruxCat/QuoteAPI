b1a162b244b35ff3b26953288de7f82f6b913255
Catherine Vu <cath.vu@gmail.com>
2021-11-04 11:49:36 -0700

    mapped quote controller to endpoint

--- init.py
+++ init.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import random
+from quotecontroller import Quote
+
 
 app = Flask(__name__)
 api = Api(app)
+
+api.add_resource(Quote, "/ai-quotes", "/ai-quotes/", "/ai-quotes/<int:id>")
+if __name__ == '__main__':
+    app.run(debug=True)
