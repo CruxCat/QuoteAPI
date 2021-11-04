60b4a86f0c3b0403d6cdb6f363bf1fb3090ef147
Catherine Vu <cath.vu@gmail.com>
2021-11-04 11:38:25 -0700

    added POST method

--- quotecontroller.py
+++ quotecontroller.py
@@ -6,3 +6,19 @@ class Quote(Resource):
             if(quote["id"] == id):
                 return quote, 200
         return "Quote not found", 404
+    def post(self, id):
+        parser = reqparse.RequestParser()
+        parser.add_argument("author")
+        parser.add_argument("quote")
+        params = parser.parse_args()
+        for quote in ai_quotes:
+            if(id == quote["id"]):
+                return f"Quote with id {id} already exists", 400
+        quote = {
+            "id": int(id),
+            "author": params["author"],
+            "quote": params["quote"]
+        }
+        ai_quotes.append(quote)
+        return quote, 201
+
